2ea07a596a53b39701521ee822a638592405e4f1
maciej <maciej.orlowski43@gmail.com>
2018-11-25 11:17:12 +0100

    MaciejOrl_sprint1_creating_project

---
+++ app/src/main/java/project/pb/bus_system_client/App.kt
@@ -0,0 +1,15 @@
+package project.pb.bus_system_client
+
+import android.app.Application
+
+class App : Application() {
+
+    val TAG = javaClass.simpleName
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
